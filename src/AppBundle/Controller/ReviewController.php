c63d34b3188006bafb2ed3d5cc2a15d571dc2c13
Julia Limousin <julia.limousin2@gmail.com>
2018-05-03 17:57:33 +0200

    début quête formulaires

--- src/AppBundle/Controller/ReviewController.php
+++ src/AppBundle/Controller/ReviewController.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Controller;
 
+//include '../Form/ReviewType.php';
+
 use AppBundle\Entity\Review;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Form\ReviewType;use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
 
@@ -20,6 +22,7 @@ class ReviewController extends Controller
      * @Route("/", name="review_index")
      * @Method("GET")
      */
+
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
@@ -40,10 +43,23 @@ class ReviewController extends Controller
     public function newAction(Request $request)
     {
         $review = new Review();
+        $form = $this->createForm(ReviewType::class, $review);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($review);
+            $em->flush();
+
+            // You can use too :
+            // return $this->redirect($this->generateUrl('review_show', array('id' => $review->getId())))
+
+            return $this->redirectToRoute('review_show', array('id' => $review->getId()));
+        }
 
         return $this->render('review/new.html.twig', array(
             'review' => $review,
+            'form' => $form->createView(),
         ));
     }
-
 }
