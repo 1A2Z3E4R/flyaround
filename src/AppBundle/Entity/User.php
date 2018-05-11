785caf2dcd74631e9d0286c94ead5d7ed87e8142
Julia Limousin <julia.limousin2@gmail.com>
2018-05-11 11:18:34 +0200

    debut de la quete fosuser

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,17 +3,18 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
 
 /**
  * User
  *
- * @ORM\Table(name="user")
+ * @ORM\Table(name="`user`")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
  */
-class User
+class User extends BaseUser
 {
     /*
-    * Adding personnal methods / variables
+    * Adding personal methods / variables
     */
 
     public function __toString()
@@ -47,7 +48,7 @@ class User
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
     /**
      * @var string
@@ -281,6 +282,7 @@ class User
      */
     public function __construct()
     {
+        parent::__construct();
         $this->reviewAuthors = new \Doctrine\Common\Collections\ArrayCollection();
     }
 
